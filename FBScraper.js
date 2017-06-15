9c44808822baa449810f6cd26daa49b5a919d823
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2017-06-15 15:58:25 -0700

    test

--- FBScraper.js
+++ FBScraper.js
@@ -1,8 +1,7 @@
 class FBScraper {
 	constructor() {
 		this.firebaseAdmin = require( 'firebase-admin' );
-		this.firebaseServiceAccount = require( './src/political-post-analysis-firebase-adminsdk-rdi0e-6781839410.js' );
-		console.log( this.firebaseServiceAccount );
+		this.firebaseServiceAccount = require( './political-post-analysis-firebase-adminsdk-rdi0e-6781839410.json' );
 		
 		this.firebaseAdmin.initializeApp( {
 			credential: this.firebaseAdmin.credential.cert( this.firebaseServiceAccount ),
