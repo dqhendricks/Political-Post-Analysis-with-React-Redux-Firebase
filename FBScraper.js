1fd31a4a66b43d8dc582cce9a460cb18978545f8
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2017-06-15 14:50:51 -0700

    test

--- FBScraper.js
+++ FBScraper.js
@@ -1,6 +1,15 @@
 class FBScraper {
+	constructor() {
+		this.admin = require( 'firebase-admin' );
+		this.serviceAccount = require( './political-post-analysis-firebase-adminsdk-rdi0e-6781839410.json' );
+		this.admin.initializeApp( {
+			credential: admin.credential.cert( serviceAccount ),
+			databaseURL: 'https://political-post-analysis.firebaseio.com'
+		} );
+	}
+	
 	start() {
-		console.log( 'test' );
+		console.log( this.admin );
 	}
 }
 
