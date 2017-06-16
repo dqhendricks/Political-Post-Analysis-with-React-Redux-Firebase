cd83cea42697e351717d363a282f9dd3ac459dc6
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2017-06-15 22:28:21 -0700

    test

--- FBScraper.js
+++ FBScraper.js
@@ -1,5 +1,6 @@
 class FBScraper {
 	constructor() {
+		// firebase
 		this.firebaseAdmin = require( 'firebase-admin' );
 		this.firebaseServiceAccount = require( './political-post-analysis-firebase-adminsdk-rdi0e-6781839410.json' );
 		
@@ -7,10 +8,36 @@ class FBScraper {
 			credential: this.firebaseAdmin.credential.cert( this.firebaseServiceAccount ),
 			databaseURL: 'https://political-post-analysis.firebaseio.com'
 		} );
+		
+		this.firebaseDatabase = this.firebaseAdmin.database();
+		// facebook
+		this.request = require( 'request' );
+		this.facebookToken = null;
 	}
 	
 	start() {
-		console.log( this.firebaseAdmin );
+		getToken( test );
+	}
+	
+	test( err, httpResponse, body ) {
+		
+	}
+	
+	facebookRequest( path, callback, method = 'GET' ) {
+		const options = {
+			url: `graph.facebook.com/${ path }` + ( ( this.facebookToken ) ? `?access_token=${ this.facebookToken }` : '' ),
+			method: method,
+			json: true
+		};
+		this.request( options, callback );
+	}
+	
+	getToken( callback ) {
+		facebookRequest( `/oauth/access_token?client_id=${ process.env.FACEBOOK_APP_ID }&client_secret=${ process.env.FACEBOOK_APP_SECRET }&grant_type=client_credentials`, ( response ) => {
+			debug.log( response );
+			this.facebookToken = response;
+			callback();
+		} );
 	}
 }
 
