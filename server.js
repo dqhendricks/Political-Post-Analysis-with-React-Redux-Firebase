0d27ebb5e43f7789a3ea94850eb51b7cfe521346
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2017-06-18 18:20:41 -0700

    tiny update

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get( '*', ( req, res ) => {
 app.listen( port );
 
 // worker
-const fbscraper = require( './FBScraper' );
+const fbScraper = require( './FBScraper' );
 
-fbscraper.start();
+fbScraper.start();
