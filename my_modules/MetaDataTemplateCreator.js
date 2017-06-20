443a88acc49b86f0b3d5ba3c2c71c4b764b5fa6c
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2017-06-20 15:48:02 -0700

    fix

--- my_modules/MetaDataTemplateCreator.js
+++ my_modules/MetaDataTemplateCreator.js
@@ -12,7 +12,7 @@ class MetaDataTemplateCreator {
 			average_comments_24: 0,
 			hourly_average_comments: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0 },
 			average_users_commenting_24: 0,
-			average_zero_like_comments: 0,
+			average_zero_like_comments: 0
 		};
 	}
 	
@@ -34,7 +34,7 @@ class MetaDataTemplateCreator {
 			total_comments_24: 0,
 			hourly_total_comments: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0 },
 			total_users_commenting: 0,
-			users_commenting: {}
+			users_commenting: {},
 			zero_like_comments: 0,
 			last_allowed_comment_time: '',
 			page_id: '',
