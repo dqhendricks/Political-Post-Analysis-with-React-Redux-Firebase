0413e8cd0ca910ed0a4bc624aa9e531a28950818
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2017-06-21 15:33:43 -0700

    finished post processing

--- my_modules/FBDataProcessor.js
+++ my_modules/FBDataProcessor.js
@@ -1,4 +1,6 @@
+const fs = require( 'fs' );
 const _ = require( 'lodash' );
+const firebaseDataStore = require( './FirebaseDataStore' );
 
 class FBDataProcessor {
 	
@@ -94,6 +96,9 @@ class FBDataProcessor {
 	
 	addComment( comment, postID, pageID ) {
 		const commentHour = this.getCommentHour( comment, postID );
+		// page
+		this.data.pages[pageID].total_comments_24++;
+		this.data.pages[pageID].hourly_total_comments_24[commentHour]++;
 		// post
 		this.data.posts[postID].total_comments_24++;
 		this.data.posts[postID].hourly_total_comments[commentHour]++;
@@ -123,6 +128,170 @@ class FBDataProcessor {
 	
 	afterScrapeProcessing() {
 		console.log( 'begin after scrape processing' );
+		// users
+		console.log( 'users' );
+		_.forIn( this.data.users, ( user, id ) => {
+			if ( user.total_comment_likes != 0 ) this.data.users[id].average_likes_per_comment = user.total_comments / user.total_comment_likes;
+			_.forIn( user.comment_starts, ( commentDuplicates, id ) {
+				if ( commentDuplicates > 1 ) this.data.users[id].duplicate_comments += commentDuplicates;
+			} );
+			delete this.data.users[id].comment_starts;
+			const affiliation_interactions = { left: false, right: false };
+			_.forIn( user.pages_interacted_with, ( value, pageID ) {
+				this.data.users[id].total_pages_interacted_with++;
+				if ( this.data.pages[pageID].affiliation_score == 1 ) {
+					affiliation_interactions.right = true;
+				} else {
+					affiliation_interactions.left = true;
+				}
+			}
+			if ( affiliation_interactions.left && affiliation_interactions.right ) this.data.users[id].both_liberal_and_conservative_interaction = true;
+			const total_affiliation_likes = user.total_conservative_likes + user.total_liberal_likes;
+			if ( total_affiliation_likes > 0 ) this.data.users[id].affiliation_score = user.total_conservative_likes / total_affiliation_likes;
+		} );
+		// posts
+		console.log( 'posts' );
+		_.forIn( this.data.posts, ( post, id ) => {
+			this.data.posts[id].total_users_commenting = _.size( post.users_commenting );
+			delete this.data.posts[id].users_commenting;
+			this.data.pages[post.from.id].total_users_commenting_24 += this.data.posts[id].total_users_commenting;
+			if ( post.total_likes = 0 ) this.data.pages[post.from.id].total_zero_like_comments_24++;
+			if ( post.total_reactions > 0 ) {
+				this.data.posts[id].love_percent = post.total_love / post.total_reactions;
+				this.data.posts[id].funny_percent = post.total_funny / post.total_reactions;
+				this.data.posts[id].wow_percent = post.total_wow / post.total_reactions;
+				this.data.posts[id].sad_percent = post.total_sad / post.total_reactions;
+				this.data.posts[id].angry_percent = post.total_angry / post.total_reactions;
+				var highestType = { type: 'n/a', count: 0 };
+				if ( post.total_love > highestType.count ) highestType = { type: 'LOVE', count: post.total_love };
+				if ( post.total_funny > highestType.count ) highestType = { type: 'WOW', count: post.total_funny };
+				if ( post.total_wow > highestType.count ) highestType = { type: 'HAHA', count: post.total_wow };
+				if ( post.total_sad > highestType.count ) highestType = { type: 'SAD', count: post.total_sad };
+				if ( post.total_angry > highestType.count ) highestType = { type: 'ANGRY', count: post.total_angry };
+				this.data.posts[id].post_type = highestType.type;
+				switch( this.data.posts[id].post_type ) {
+					case 'LOVE':
+						this.data.pages[post.page_id].total_love_posts++;
+						break;
+					case 'WOW':
+						this.data.pages[post.page_id].total_funny_posts++;
+						break;
+					case 'HAHA':
+						this.data.pages[post.page_id].total_wow_posts++;
+						break;
+					case 'SAD':
+						this.data.pages[post.page_id].total_sad_posts++;
+						break;
+					case 'ANGRY':
+						this.data.pages[post.page_id].total_angry_posts++;
+						break;
+				}
+			}
+		} );
+		// pages
+		console.log( 'pages' );
+		_.forIn( this.data.pages, ( page, id ) => {
+			if ( page.total_posts != 0 ) this.data.pages[id].average_comments_24 = page.total_comments_24 / page.total_posts;
+			if ( page.total_posts != 0 ) this.data.pages[id].average_users_commenting_24 = page.total_users_commenting_24 / page.total_posts;
+			if ( page.total_posts != 0 ) this.data.pages[id].average_zero_like_comments_24 = page.total_zero_like_comments_24 / page.total_posts;
+			var highestType = { type: 'n/a', count: 0 };
+			if ( page.total_love_posts > highestType.count ) highestType = { type: 'LOVE', count: page.total_love_posts };
+			if ( page.total_funny_posts > highestType.count ) highestType = { type: 'WOW', count: page.total_funny_posts };
+			if ( page.total_wow_posts > highestType.count ) highestType = { type: 'HAHA', count: page.total_wow_posts };
+			if ( page.total_sad_posts > highestType.count ) highestType = { type: 'SAD', count: page.total_sad_posts };
+			if ( page.total_angry_posts > highestType.count ) highestType = { type: 'ANGRY', count: page.total_angry_posts };
+			this.data.pages[id].mostly_posts_type = highestType.type;
+			_.forIn( page.hourly_total_comments_24, ( commentTotal, id ) => {
+				if ( page.total_posts != 0 ) this.data.pages[id].hourly_average_comments_24[id] = commentTotal / page.total_posts;
+			} );
+		} );
+		// totals
+		console.log( 'toals' );
+		this.data.totals.pages = _.size( this.data.pages );
+		console.log( `pages total: ${ this.data.totals.pages }` );
+		this.data.totals.posts = _.size( this.data.posts );
+		console.log( `posts total: ${ this.data.totals.posts }` );
+		this.data.totals.users = _.size( this.data.users );
+		console.log( `users total: ${ this.data.totals.users }` );
+		this.data.totals.post_reactions = _.size( this.data.post_reactions );
+		console.log( `post_reactions total: ${ this.data.totals.post_reactions }` );
+		this.data.totals.comments = _.size( this.data.comments );
+		console.log( `comments total: ${ this.data.totals.comments }` );
+		
+		const oldFileName = './yesterdays_data.json';
+		const newFileName = './todays_data.json';
+		// save meta data to file
+		console.log( 'save meta data to file' );
+		fs.writeFileSync( newFileName, JSON.stringify( this.data, null, 2 ) , 'utf-8' );
+		delete this.data.post_reactions;
+		delete this.data.comments;
+		
+		// save meta data to database
+		console.log( 'save meta data to database' );
+		const updateData = {};
+		this.convertCollectionToFirebaseUpdates( this.data, updateObject );
+		firebaseDataStore.update( updateData );
+		delete this.data.totals;
+		delete this.data.pages;
+		delete this.data.posts;
+		updateData = {};
+		
+		// load last update's meta data
+		console.log( 'load last update\'s meta data' );
+		this.oldData = null;
+		if ( fs.existsSync( oldFileName ) ) {
+			this.oldData = require( oldFileName );
+		}
+		
+		// compare users and find orphaned users for deletion
+		console.log( 'compare users and find orphaned users for deletion' );
+		if ( this.oldData ) {
+			_.forIn( this.data.users, ( user, id ) => {
+				delete this.oldData.users[id];
+			} );
+		}
+		delete this.data;
+		
+		// delete old data
+		console.log( 'delete old data' );
+		if ( this.oldData ) {
+			updateData = {};
+			convertCollectionToFirebaseDeletions( this.oldData.pages, updateObject, 'pages' );
+			convertCollectionToFirebaseDeletions( this.oldData.posts, updateObject, 'posts' );
+			convertCollectionToFirebaseDeletions( this.oldData.users, updateObject, 'users' );
+			convertCollectionToFirebaseDeletions( this.oldData.post_reactions, updateObject, 'post_reactions' );
+			convertCollectionToFirebaseDeletions( this.oldData.comments, updateObject, 'comments' );
+			firebaseDataStore.update( updateData );
+		}
+		
+		// delete old data file and rename new data file
+		console.log( 'delete old data file and rename new data file' );
+		if ( this.oldData ) {
+			fs.unlinkSync( oldFileName );
+			fs.renameSync( newFileName, oldFileName );
+		}
+		
+		console.log( 'cycle finished' );
+	}
+	
+	convertCollectionToFirebaseUpdates( collection, updateObject, startPath = '' ) {
+		_.forIn( collection, ( collectionItem, key ) => {
+			if ( this.isObject( collectionItem ) ) {
+				this.convertCollectionToFirebaseUpdates( collectionItem, updateObject, `${ startPath }/${ key }` );
+			} else {
+				updateObject[`${ startPath }/${ key }`] = collectionItem;
+			}
+		} );
+	}
+	
+	isObject( reference ) {
+		return ( reference !== null && typeof reference === 'object' );
+	}
+	
+	convertCollectionToFirebaseDeletions( collection, updateObject, startPath = '' ) {
+		_.forIn( collection, ( collectionItem, key ) => {
+			updateObject[`${ startPath }/${ key }`] = {};
+		} );
 	}
 }
 
--- my_modules/MetaDataTemplateCreator.js
+++ my_modules/MetaDataTemplateCreator.js
@@ -8,11 +8,17 @@ class MetaDataTemplateCreator {
 			total_wow_posts: 0,
 			total_sad_posts: 0,
 			total_angry_posts: 0,
+			mostly_posts_type: 'n/a',
 			hourly_posts: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0 },
+			total_comments_24: 0,
+			hourly_total_comments_24: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0 },
 			average_comments_24: 0,
-			hourly_average_comments: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0 },
+			hourly_average_comments_24: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0 }, //
+			total_users_commenting_24: 0,
 			average_users_commenting_24: 0,
-			average_zero_like_comments: 0
+			total_zero_like_comments_24: 0,
+			average_zero_like_comments_24: 0,
+			affiliation_score: 0
 		};
 	}
 	
@@ -30,7 +36,7 @@ class MetaDataTemplateCreator {
 			sad_percent: 0,
 			total_angry: 0,
 			angry_percent: 0,
-			highest_percent: '',
+			post_type: 'n/a',
 			total_comments_24: 0,
 			hourly_total_comments: { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0 },
 			total_users_commenting: 0,
