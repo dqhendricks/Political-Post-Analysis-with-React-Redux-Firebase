d7743c2b787ba39f780e0484f60296bd95ad1b50
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2017-06-24 16:30:29 -0700

    remove uneeded dependencies

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,10 @@
-import firebase from 'firebase';
 import axios from 'axios';
 
 export const FETCH_PAGES = 'fetch_pages';
 
-const firebaseConfig = {
-	apiKey: '',
-	authDomain: 'political-post-analysis.firebaseio.com',
-	databaseURL: 'https://political-post-analysis.firebaseio.com/'
-};
-firebase.initializeApp( firebaseConfig );
-const database = firebase.database();
-
 export function fetchPages() {
-	return dispatch => {
-		database.ref( 'pages' ).on( 'value', snapshot => {
-			dispatch( {
-				type: FETCH_PAGES,
-				payload: snapshot.val()
-			} );
-		} );
+	return {
+		type: FETCH_PAGES,
+		payload: {}
 	}
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'; // allows url routing
 import promise from 'redux-promise'; // middleware waits for ajax response
-import thunk from 'redux-thunk'; // allow async action creators (return functions in action creator)
 
 import reducers from './reducers';
 import PageIndex from './components/page-index';
 
-const createStoreWithMiddleware = applyMiddleware( promise, thunk )( createStore );
+const createStoreWithMiddleware = applyMiddleware( promise )( createStore );
 
 ReactDOM.render(
 	<Provider store={ createStoreWithMiddleware( reducers ) }>
