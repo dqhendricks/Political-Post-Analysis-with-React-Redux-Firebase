f804cb4790992f01df95d94490a2d2e3eaeb203e
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2017-07-01 22:58:05 -0700

    test

--- my_modules/FacebookScraper.js
+++ my_modules/FacebookScraper.js
@@ -12,12 +12,14 @@ class FacebookScraper {
 	start() {
 		// do first iteration at midnight UTC, then every 24 hours after that
 		this.iteration();
+		/*
 		setTimeout( () => {
 			this.iteration();
 			setInterval( () => {
 				this.iteration();
 			}, 1000 * 60 * 60 * 24 );
 		}, this.millisecondsTillStartTime() );
+		*/
 	}
 	
 	millisecondsTillStartTime() {
