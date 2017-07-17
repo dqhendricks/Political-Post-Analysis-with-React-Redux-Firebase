5a2536222f7a361f68d9ddecb2f615878b3a4774
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2017-07-17 10:38:55 -0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,8 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     contentBase: './public'
-  }
+  },
+  plugins = [
+        new webpack.EnvironmentPlugin(['NODE_ENV'])
+    ]
 };
